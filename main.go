9f0cc7d23873cb1be588e1508b3ac19dfcff03ed
Neil Johnson <neil@matrix.org>
2019-02-26 15:58:48 +0000

    set default type for python_version

--- main.go
+++ main.go
@@ -231,7 +231,7 @@ func createTable(db *sql.DB) error {
 		daily_user_type_native BIGINT,
 		daily_user_type_bridged BIGINT,
 		daily_user_type_guest BIGINT,
-		python_version TEXT
+		python_version TEXT DEFAULT ‘UNKNOWN'
 		)`)
 
 	return err
